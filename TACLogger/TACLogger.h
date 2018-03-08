555d4e05ed3d31e8ce57c87ac58f36823aac35d9
TACME <tacme@TACMEs-Mac-mini.local>
2018-03-08 10:54:51 +0400

    Initial Commit

---
+++ TACLogger/TACLogger.h
@@ -0,0 +1,19 @@
+//
+//  TACLogger.h
+//  TACLogger
+//
+//  Created by TACME on 3/8/18.
+//  Copyright © 2018 TACME. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TACLogger.
+FOUNDATION_EXPORT double TACLoggerVersionNumber;
+
+//! Project version string for TACLogger.
+FOUNDATION_EXPORT const unsigned char TACLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TACLogger/PublicHeader.h>
+
+
